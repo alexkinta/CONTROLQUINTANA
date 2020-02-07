2365b2f7e78e1687bf7982d0d02be7e44e00c56f
dam <dam@203P01.intern>
2020-02-07 09:19:45 +0100

    2 version

--- Versiones/src/versiones/Clase1.java
+++ Versiones/src/versiones/Clase1.java
@@ -16,7 +16,7 @@ public class Clase1 {
      */
     public static void main(String[] args) {
         System.out.println("hola");
-        System.out.println("adios");
+        System.out.println("hola");
     }
     
 }
